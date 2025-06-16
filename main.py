b0a577af7d2670897caf0627bbc30782ffb093af
BlackCoder56 <blackcoda56@gmail.com>
2025-06-16 17:54:42 +0300

    methods in OOP

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 class house():
+
+    company_name = "Awesome building comppany"
+
+    inflation_coefficient = 1.08
+
     def __init__(self, address, state, Alarm, price):
+        """ The self let's you know that a certain attribute changes from object to object. """
         self.address = address
         self.state = state
         self.Alarm  = Alarm
         self.price = price
 
-house1 = house(123, "california", False, 300000)
-house2 = house(5678, "texas", True, 100000)
+    def correctPriceMethod(self):
+        self.price = self.price * self.inflation_coefficient
 
-print(house1.state)
-print(house2.price)
+apartment1 = house(123, "california", False, 300000)
+apartment2 = house(5678, "california", False, 300000)
+apartment3price = 300000
+
+# ----------------------------------
+apartment1.correctPriceMethod()
+
+# -----------------------------------
+house.correctPriceMethod(apartment2)
+
+# -----------------------------------
+def correctPricefunction(apartment):
+    return apartment * 1.08
+
+print(apartment3price)
+
+apartment3price = correctPricefunction(apartment3price)
+print(apartment3price)
